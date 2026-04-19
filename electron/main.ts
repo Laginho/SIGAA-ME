4e7b4c389a8103100f23895da90ee0992fee9009
Bruno <sonbruno1@gmail.com>
2026-04-19 12:14:58 -0300

    fix: apply the custom icon for the actual app instead of the installer

--- electron/main.ts
+++ electron/main.ts
@@ -63,7 +63,7 @@ const sigaaService = new SigaaService()
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(process.env.VITE_PUBLIC, 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
     },
